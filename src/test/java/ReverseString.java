04702994696abac5ff2a016db671920c739caa6c
MaxGB2605 <maximbrazhko@gmail.com>
2024-02-14 20:02:18 -0500

    third commit

---
+++ src/test/java/ReverseString.java
@@ -0,0 +1,11 @@
+public class ReverseString {
+    public static void main(String[] args) {
+        String s = "hello world";
+        String rev = "";
+
+        for (int i = 0; i < s.length(); i++) {
+        rev = s.charAt(i)+rev;
+        }
+        System.out.println(rev);
+    }
+}
